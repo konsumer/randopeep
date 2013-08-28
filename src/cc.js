30a0cc3c387aaf2fc2b7f95a8effe34a43c7d35d
David Konsumer <konsumer@jetboystudio.com>
2013-08-28 03:46:24 -0700

    improved algorythm

--- src/cc.js
+++ src/cc.js
@@ -1,78 +1,65 @@
 'use strict';
 
 module.exports = function(randopeep){
+	
+	var luhnGen = (function(){
+		var d = [0, 1, 2, 3, 4, -4, -3, -2, -1, 0];
+		return function (l,s,i,m) {
+			s = 0;
+			for (i=0; i<l.length; i++ ){ s += parseInt(l.substring(i,i+1),10); }
+			for (i=l.length-1; i>=0; i-=2 ){ s += d[parseInt(l.substring(i, i + 1),10)]; }
+			m = 10 - (s % 10);
+			return (m === 10) ? 0 : m;
+		};
+	})();
+
 	/**
 	 * Generate a credit-card #
-	 * @param  String issuer  Issuer of CC #: visa (default), mastercard, amex, discover
+	 * http://en.wikipedia.org/wiki/Bank_card_number#Issuer_identification_number_.28IIN.29
+	 * @param  String issuer  Issuer of CC #: visa (default), mastercard, amex, discover, diners
 	 * @param Number len      For visa & mastercard, can be 13 or 16 (default)
 	 * @return String         Your fake CC #
 	 */
 	return function(issuer, len){
 		issuer = issuer || 'visa';
 		len = len || 16;
-
-		var pos;
-		var str = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-		var sum = 0;
-		var finalDigit = 0;
-		var t = 0;
-		var lenOffset = 0;
-
+		if (['visa', 'mastercard', 'amex', 'discover', 'diners'].indexOf(issuer) === -1){
+			return false;
+		}
+		var out;
 
 		if (issuer === 'visa'){
-			str[0] = 4;
-			pos = 1;
-		}
-
-		if (issuer === 'mastercard'){
-			str[0] = 4;
-			pos = 1;
-		}
-
-		if (issuer === 'amex'){
-			str[0] = 3;
-			t = Math.floor(Math.random() * 4) % 4;
-			str[1] = 4 + t;	  // Between 4 and 7.
-			pos = 2;
-			len = 15;
-		}
-
-		if (issuer === 'discover'){
-			str[0] = 6;
-			str[1] = 0;
-			str[2] = 1;
-			str[3] = 1;
-			pos = 4;
+			if (len !== 16 && len !== 13){
+				len = 16;
+			}
+			out = 4;
+		}else{
 			len = 16;
 		}
 
-		// Fill all the remaining numbers except for the last one with random values.
-		while (pos < len - 1) {
-			str[pos++] = Math.floor(Math.random() * 10) % 10;
+		if (issuer === 'mastercard'){
+			out = randopeep.randomEl([51, 52, 53, 54, 55]);
 		}
 
-		// Calculate the Luhn checksum of the values thus far.
-		lenOffset = (len + 1) % 2;
-		for (pos = 0; pos < len - 1; pos++) {
-			if ((pos + lenOffset) % 2) {
-				t = str[pos] * 2;
-				if (t > 9) {
-					t -= 9;
-				}
-				sum += t;
-			}
-			else {
-				sum += str[pos];
-			}
+		if (issuer === 'amex'){
+			out = randopeep.randomEl([34, 37]);
 		}
 
-		// Choose the last digit so that it causes the entire string to pass the checksum.
-		finalDigit = (10 - (sum % 10)) % 10;
-		str[len - 1] = finalDigit;
-	 
-		// Output the CC value.
-		t = str.join('');
-		t = t.substr(0, len);
-		return t;
+		if (issuer === 'discover'){
+			out = randopeep.randomEl([6011, 622126+randopeep.int(799), 644+randopeep.int(5), 65]);
+		}
+
+		if (issuer === 'diners'){
+			out = randopeep.randomEl([54, 55]);
+		}
+
+		out = out.toString().split('');
+		while (out.length < (len-1)){
+			out.push(randopeep.int().toString());
+		}
+
+		out.push(luhnGen(out.join('')));
+		return out.join('');
+
 	};
 };
