016356eca1cedcafc739e9142e4b852e1bc4699d
David Konsumer <konsumer@jetboystudio.com>
2017-03-29 21:16:20 -0700

    fixed corporate

--- src/corporate.js
+++ src/corporate.js
@@ -1,10 +1,10 @@
-import { get, format } from './index'
+import { get, format, randomEl } from './index'
 import getName from './name'
 
 export const name = (frm, n = 1) => {
   if (n !== 1) { return (new Array(n)).fill('').map(() => name(frm)) }
-  const sOrigins = get('address/origins')
-  frm = frm || get('corporate/origins')
+  const origin = get('corporate/origins')
+  frm = frm || randomEl(['cyber', 'large', 'small'])
   switch (frm) {
     case 'cyber':
       return format(
@@ -13,24 +13,23 @@ export const name = (frm, n = 1) => {
           get('corporate/cyber')
         )
     case 'firm':
-        // they are all from the same origin
-      var origin = get('corporate/origins')
+      // they are all from the same origin
       return format(
           '{0}, {1} and {2}',
-          getName({'justLast': true, 'origin': origin, 'prefix': false, suffix: false}),
-          getName({'justLast': true, 'origin': origin, 'prefix': false, suffix: false}),
-          getName({'justLast': true, 'origin': origin, 'prefix': false, suffix: false})
+          getName({justLast: true, origin, prefix: false, suffix: false}),
+          getName({justLast: true, origin, prefix: false, suffix: false}),
+          getName({justLast: true, origin, prefix: false, suffix: false})
         )
     case 'small':
       return format(
           '{0} {1}',
-          getName({'justLast': true, 'origin': sOrigins, 'prefix': false, suffix: false}),
+          getName({justLast: true, origin, prefix: false, suffix: false}),
           get('corporate/small')
         )
     case 'large':
       return format(
           '{0} {1}',
-          getName({'justLast': true, 'origin': sOrigins, 'prefix': false, suffix: false}),
+          getName({justLast: true, origin, prefix: false, suffix: false}),
           get('corporate/large')
         )
   }
